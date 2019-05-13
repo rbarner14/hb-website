39e690849ff5bb1227071166c4d0f3faf2a30a45
Madelaine Bolton <38406261+mbolts@users.noreply.github.com>
2019-05-13 15:35:50 -0700

    updating findAVenue (#10)
    * test message
    
    * created base server file, template folder and requirements.txt
    
    * removed test message
    
    * add base.html
    
    * updated findAVenue with DogFriendly

--- findAVenue.py
+++ findAVenue.py
@@ -40,23 +40,23 @@ def getGeocodeLocation(inputString):
 # This function takes in a string representation of a location and cuisine type, 
 # geocodes the location, and then passes in the latitude and longitude 
 # coordinates to a string that becomes the Foursquare API GET request.
-def findARestaurant(mealType, location):
+def findARestaurant(features, location):
 
     # Parse tuple resulting from the return value of the getGeocodeLocation
     # function with the location of search passed into it called.  Bind 
     # latitude to the first item in tuple and longitude to the 2nd.
     latitude, longitude = getGeocodeLocation(location)
     # Create URL with latitude, longitude, API keys and meal type.
-    url = ('https://api.foursquare.com/v2/venues/search?client_id=%s&client_secret=%s&v=20130815&ll=%s,%s&query=%s' % (foursquare_client_id, foursquare_client_secret,latitude,longitude,mealType))
+    url = (f'https://api.foursquare.com/v2/venues/search?client_id={foursquare_client_id}&client_secret={foursquare_client_secret}&v=20190513&ll={latitude},{longitude}&features={features}')
     h = httplib2.Http()
-    result = json.loads(h.request(url,'GET')[1])
+    result = json.loads(h.request(url, 'GET')[1])
 
     # Return restaraunt info diction if a restaraunt that matches search exists;
     # return "No Restaraunts Found" otherwise.
     if result['response']['venues']:
         # Grab the first restaurant, its id, name and address.
         restaurant = result['response']['venues'][0]
-        venue_id = restaurant['id'] 
+        venue_id = restaurant['id']
         restaurant_name = restaurant['name']
         restaurant_address = restaurant['location']['formattedAddress']
         # Format the Restaurant Address into one string.
@@ -68,8 +68,8 @@ def findARestaurant(mealType, location):
         restaurant_address = address
         
         # Get a  300x300 picture of the restaurant using the venue_id (you can change this by altering the 300x300 value in the URL or replacing it with 'orginal' to get the original picture
-        url = ('https://api.foursquare.com/v2/venues/%s/photos?client_id=%s&v=20150603&client_secret=%s' % ((venue_id,foursquare_client_id,foursquare_client_secret)))
-        result = json.loads(h.request(url,'GET')[1])
+        url = (f'https://api.foursquare.com/v2/venues/{venue_id}/photos?client_id={foursquare_client_id}&v=20190513&client_secret={foursquare_client_secret}')
+        result = json.loads(h.request(url, 'GET')[1])
         
         # Grab the first image.
         # If no image available, insert default image url.
@@ -81,26 +81,20 @@ def findARestaurant(mealType, location):
         else:
             imageURL = "http://pixabay.com/get/8926af5eb597ca51ca4c/1433440765/cheeseburger-34314_1280.png?direct"
 
-        restaurantInfo = {'name':restaurant_name, 'address':restaurant_address, 'image':imageURL}
-        #print "Restaurant Name: %s " % restaurantInfo['name']
-        #print "Restaurant Address: %s " % restaurantInfo['address']
-        #print "Image: %s \n " % restaurantInfo['image']
+        restaurantInfo = {'name': restaurant_name,
+                          'address': restaurant_address,
+                          'image': imageURL}
+        # print "Restaurant Name: %s " % restaurantInfo['name']
+        # print "Restaurant Address: %s " % restaurantInfo['address']
+        # print "Image: %s \n " % restaurantInfo['image']
         print(restaurantInfo)
         return restaurantInfo
     else:
-        #print "No Restaurants Found for %s" % location
+        # print "No Restaurants Found for %s" % location
         return "No Restaurants Found"
 
 
-################################################################################
+###############################################################################
 # Enable running at command line.
 if __name__ == '__main__':
-    findARestaurant("Pizza", "Tokyo, Japan")
-    findARestaurant("Tacos", "Jakarta, Indonesia")
-    findARestaurant("Tapas", "Maputo, Mozambique")
-    findARestaurant("Falafel", "Cairo, Egypt")
-    findARestaurant("Spaghetti", "New Delhi, India")
-    findARestaurant("Cappuccino", "Geneva, Switzerland") 
-    findARestaurant("Sushi", "Los Angeles, California")
-    findARestaurant("Steak", "La Paz, Bolivia")
-    findARestaurant("Gyros", "Sydney Austrailia")
+    findARestaurant('13', "Tokyo, Japan")
